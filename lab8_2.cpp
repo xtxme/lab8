b16e5cc5fbcc2a8b1c5747ecf4a0b386e733647c
Tanapron Tangpadungsuk <tt.icy013@gmail.com>
2023-12-15 15:27:10 +0700

    lab8.2 complete

--- lab8_2.cpp
+++ lab8_2.cpp
@@ -3,7 +3,32 @@
 #include<string>
 using namespace std;
 
-//[Missing Code 1] Write definition of the function findGrade() here.
+char findGrade(double grades)
+{
+	if (grades > 90)
+	{
+		return 'A';
+	}
+	if (grades > 75)
+	{
+		return 'B';
+	}
+	if (grades > 60)
+	{
+		return 'C';
+	}
+	if (grades > 45)
+	{
+		return 'D';
+	}
+	if (grades <= 45)
+	{
+		return 'F';
+	}
+	else
+	return 0;
+}
+
 
 int main(){
 	//Input the number of students
@@ -17,9 +42,9 @@ int main(){
 	while(i < N){
 		cout << "Name of student " << i+1 << ": ";
 		cin.ignore();
-		//[Missing Code 2] Get name of the i-th students that may include whitespace.
+		getline(cin,name[i]);
 		cout << "Score of student " << i+1 << ": ";
-		//[Missing Code 3] Get score of the i-th students.
+		cin >> score[i];
 		i++;
 	}
 	
--- lab8_3.cpp
+++ lab8_3.cpp
@@ -1,3 +1,58 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+    int age, height, bounty;
+    cout << "Enter your age: ";
+    cin >> age;
+
+    if (age <= 25)
+    {
+        cout << "Enter your height: ";
+        cin >> height;
+        if (height < 100)
+        {
+            cout << "character = " << "Chopper";
+        }else{
+            if (height < 180)
+            {
+                cout << "character = " << "Usopp";
+            }else{
+                cout << "Enter your bounty: ";
+                cin >> bounty;
+                if (bounty > 1100000000)
+                {
+                    cout << "character = " << "Zoro";
+                }else{
+                    cout << "character = " << "Sanji";
+                }
+                
+            }
+            
+        }
+        
+    }else if (age <= 60)
+    {
+        cout << "Enter your bounty: ";
+        cin >> bounty;
+        if ( bounty > 500000000)
+        {
+            cout << "character = " << "Jinbe";
+        }else{
+            cout << "character = " << "Franky";
+        }
+        
+    }else{
+        cout << "character = " << "Brook";
+    }
+    
+    
+    
+
+    
+}
 /*
 "Enter your age: "
 "Enter your height: "
